5d97080515ff2b62e4bc026e3768270b584d56c8
shalene-rgb <wagurashalene@gmail.com>
2026-07-06 07:59:38 -0700

    Change database configuration to Aiven cloud
    Updated database connection details for Aiven cloud MySQL.

--- config.php
+++ config.php
@@ -1,15 +1,14 @@
 <?php
-// Database configuration credentials
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', ''); // XAMPP default password is blank
-define('DB_NAME', 'sacco_db');
+define('DB_SERVER', 'mysql-22719b9d-wagurashalene-8fb2.k.aivencloud.com');
+define('DB_USERNAME', 'avnadmin');
+define('DB_PASSWORD', 'AVNS_3rGSzYmpfheAKGHJVKQ');
+define('DB_NAME', 'defaultdb');
+define('DB_PORT', '11340');
 
-// Attempt to connect to the MySQL database
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+// Connect to the Aiven cloud MySQL database instance using the explicit port
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
 
-// Check the connection
 if($link === false){
-    die("ERROR: Could not connect to the database. " . mysqli_connect_error());
+    die("ERROR: Could not connect to the cloud database. " . mysqli_connect_error());
 }
 ?>
